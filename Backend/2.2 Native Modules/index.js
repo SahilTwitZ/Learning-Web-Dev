1abf569c78fbadccd3e87db270e0e53e6accb4c1
Shahil <sahil321nis@gmail.com>
2024-10-28 11:41:33 +0530

    Node JS start

---
+++ Backend/2.2 Native Modules/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+fs.writeFile("message2.txt", "Hello file system!", (err) => {
+    if (err) throw err;
+    console.log("File written successfully");
+});
+
+fs.readFile("message2.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
